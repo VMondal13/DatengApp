444869235ceb5839eca186314efc30d41dc979a4
V M <vishalmondal13@gmail.com>
2024-06-26 19:48:48 +0530

    End of section 9

--- client/src/app/members/members-detail/members-detail.component.ts
+++ client/src/app/members/members-detail/members-detail.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/Member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-members-detail',
   templateUrl: './members-detail.component.html',
   styleUrls: ['./members-detail.component.css']
 })
-export class MembersDetailComponent {
+export class MembersDetailComponent implements OnInit{
+  member: Member | undefined
+  
+  constructor(private memberService: MembersService, private route: ActivatedRoute){
 
+  }
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    const username = this.route.snapshot.paramMap.get('username');
+    if(!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => {
+        this.member = member;
+        console.log(member);
+      }
+    })
+  }
 }
